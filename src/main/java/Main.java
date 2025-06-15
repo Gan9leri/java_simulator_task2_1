a1d3359376363880854848ee5d8bc8ed5377d697
Gan9leri <master980@yandex.ru>
2025-06-15 18:09:42 +0300

    add Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] agrs){
+        String result;
+        // result = "Привет, Мир";
+        // result = "Привет Мир!";
+        // result = "Привет Мир";
+        result = "Привет, Мир!";
+        System.out.print(result);
+    }
+}
